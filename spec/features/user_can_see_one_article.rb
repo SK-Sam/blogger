f4b7be95c015e4668d08bfb72e56b6a307ff5c82
Sam Yeo <yeosamuel95@gmail.com>
2021-01-26 01:54:02 -0700

    Add comment section to articles show page as partial

--- spec/features/user_can_see_one_article.rb
+++ spec/features/user_can_see_one_article.rb
@@ -5,6 +5,8 @@ describe "user sees one article" do
     it "displays information for one article" do
       article_1 = Article.create(title: "Sample Title 1", body: "Sample Body Text 1")
       article_1 = Article.create(title: "Sample Title 2", body: "Sample Body Text 2")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
       visit articles_path
       click_link article_1.title
